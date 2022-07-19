60cf72d0ade0979adea87e64c01b4473e447adf0
o.pochic@gmail.com <o.pochic@gmail.com>
2022-07-19 18:51:59 +0200

    Installation + Base Dashboarf/Preferences/Login créés

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
-import Home from "@pages/Home";
-
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Dashboard from "@components/dashboard/Dashboard";
+import Preferences from "@components/preferences/Preferences";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <main>
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+          <Routes>
+            <Route path="/preferences" element={<Preferences />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
