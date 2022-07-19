578b0b506ecba59b66733b22d38ec064d2aab4e1
o.pochic@gmail.com <o.pochic@gmail.com>
2022-07-19 19:08:05 +0200

    Set of color theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import React, { useState } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Dashboard from "@components/dashboard/Dashboard";
 import Preferences from "@components/preferences/Preferences";
+import Login from "@components/login/Login";
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
